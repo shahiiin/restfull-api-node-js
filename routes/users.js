02fad67ebfc4d03b89bbd09b1dba3b0ff0fa33ff
shahin <shahin.shahi1986@gmail.com>
2023-03-04 23:19:40 +0330

    upload simple project

---
+++ routes/users.js
@@ -0,0 +1,51 @@
+import express from "express";
+import { v4 as uuidv4 } from "uuid";
+
+const router = express.Router();
+
+let users = [];
+
+router.get("/", (req, res) => {
+  res.send(users);
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+
+  const foundUser = users.find((user) => user.id == id);
+
+  res.send(foundUser);
+});
+
+router.post("/", (req, res) => {
+  const user = req.body;
+
+  const Id = uuidv4();
+  const UserId = { ...user, id: Id };
+
+  users.push(UserId);
+  res.send(`User with the name ${user.firstName} added!`);
+});
+
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+
+  users = users.filter((user) => user.id != id);
+
+  res.send(`User with the id ${id} deleted.`);
+});
+
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const { firstName, lastName, city, age } = req.body;
+
+  const user = users.find((user) => user.id == id);
+
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (city) user.city = city;
+  if (age) user.age = age;
+
+  res.send(`User with the id ${id} has been changed!`);
+});
+export default router;
